9ed0b186559402a89348d199d252971adac4d822
Quan Tran <quantran@Quans-MacBook-Pro.local>
2020-06-12 13:43:05 +0700

    install ext

---
+++ vs-ext.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# ~/osx.sh — Originally from https://mths.be/osx
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `osx.sh` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+###############################################################################
+# uninstall vs extention                                                      #
+###############################################################################
+
+rm -rf ~/.vscode/extensions
+
+###############################################################################
+# install vs extention                                                      #
+###############################################################################
+
+code --install-extension esbenp.prettier-vscode
+code --install-extension eamodio.gitlens
+code --install-extension  techer.open-in-browser
+code --install-extension wayou.vscode-todo-highlight
